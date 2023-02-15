98304a484811a358ac7d49c0e81778d50ab5bf7c
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-15 12:43:08 -0500

    coaches list and list item

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,16 +6,27 @@
 			<router-link to="/register">Register as Coach</router-link>
 		</div>
 		<ul v-if="hasCoaches">
-			<li v-for="coach in filterCoaches" :key="coach.id">
-				{{ coach.firstName }}
-			</li>
+			<coach-item
+				v-for="coach in filterCoaches"
+				:key="coach.id"
+				:id="coach.id"
+				:first-name="coach.firstName"
+				:last-name="coach.lastName"
+				:rate="coach.hourlyRate"
+				:areas="coach.areas"
+			></coach-item>
 		</ul>
 		<h3 v-else>No coaches found.</h3>
 	</section>
 </template>
 
 <script>
+import CoachItem from '../../components/coaches/CoachItem.vue';
+
 export default {
+	components: {
+		CoachItem,
+	},
 	computed: {
 		filterCoaches() {
 			return this.$store.getters['coaches/coachesGetter']; //namespace/getter name
@@ -26,3 +37,16 @@ export default {
 	},
 };
 </script>
+
+<style lang="scss" scoped>
+ul {
+	padding: 0;
+	margin: 0;
+	list-style: none;
+}
+
+.controls {
+	display: flex;
+	justify-content: space-between;
+}
+</style>

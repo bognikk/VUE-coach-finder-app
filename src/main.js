3b39a072415ade026879c7de36eb73973f365295
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-16 18:23:23 -0500

    loading spinner

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
 import BaseButton from './components/UI/BaseButton.vue';
 import BaseBadge from './components/UI/BaseBadge.vue';
+import BaseSpinner from './components/UI/BaseSpinner.vue';
 
 const app = createApp(App);
 
@@ -15,5 +16,6 @@ app.use(store);
 app.component('base-card', BaseCard);
 app.component('base-button', BaseButton);
 app.component('base-badge', BaseBadge);
+app.component('base-spinner', BaseSpinner);
 
 app.mount('#app');
--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -6,11 +6,14 @@
 		<base-card>
 			<div class="controls">
 				<base-button mode="outline" @click="loadCoaches">Refresh</base-button>
-				<base-button v-if="!isCoach" link to="/register"
+				<base-button v-if="!isCoach && !isLoading" link to="/register"
 					>Register as Coach</base-button
 				>
 			</div>
-			<ul v-if="hasCoaches">
+			<div v-if="isLoading">
+				<base-spinner></base-spinner>
+			</div>
+			<ul v-else-if="hasCoaches">
 				<coach-item
 					v-for="coach in filteredCoaches"
 					:key="coach.id"
@@ -37,6 +40,7 @@ export default {
 	},
 	data() {
 		return {
+			isLoading: false,
 			activeFilters: {
 				frontend: true,
 				backend: true,
@@ -64,15 +68,17 @@ export default {
 			});
 		},
 		hasCoaches() {
-			return this.$store.getters['coaches/hasCoaches'];
+			return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
 		},
 	},
 	methods: {
 		setFilters(updatedFilters) {
 			this.activeFilters = updatedFilters;
 		},
-		loadCoaches() {
-			this.$store.dispatch('coaches/loadCoaches');
+		async loadCoaches() {
+			this.isLoading = true;
+			await this.$store.dispatch('coaches/loadCoaches');
+			this.isLoading = false;
 		},
 	},
 	created() {

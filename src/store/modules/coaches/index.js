6d26ce3d37b006098e875c0939291fc3d1852ea7
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-16 11:59:47 -0500

    form validation

--- src/store/modules/coaches/index.js
+++ src/store/modules/coaches/index.js
@@ -9,20 +9,20 @@ export default {
       coaches: [
         {
           id: 'c1',
-          firstName: 'Maximilian',
-          lastName: 'Schwarzmüller',
+          firstName: 'John',
+          lastName: 'Smith',
           areas: ['frontend', 'backend', 'career'],
           description:
-            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+            "I'm John and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
           hourlyRate: 30,
         },
         {
           id: 'c2',
-          firstName: 'Julie',
+          firstName: 'Jane',
           lastName: 'Jones',
           areas: ['frontend', 'career'],
           description:
-            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+            'I am Jane and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
           hourlyRate: 30,
         },
       ],

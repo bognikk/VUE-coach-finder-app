4fd69e1cdb2392a6af9fee76ef9e5574867bdc36
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-21 11:13:58 -0500

    attaching the token to outgoing requests

--- src/store/modules/coaches/actions.js
+++ src/store/modules/coaches/actions.js
@@ -9,8 +9,11 @@ export default {
       areas: data.areas,
     };
 
+    const token = context.rootGetters.token;
+
     const response = await fetch(
-      `https://coach-finder-28b14-default-rtdb.firebaseio.com/coaches/${userId}.json`,
+      `https://coach-finder-28b14-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=` +
+        token,
       {
         method: 'PUT',
         body: JSON.stringify(coachData),
--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -30,8 +30,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://coach-finder-28b14-default-rtdb.firebaseio.com/requests/${coachId}.json`
+      `https://coach-finder-28b14-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 

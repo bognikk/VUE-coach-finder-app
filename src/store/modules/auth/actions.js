dca88b2a7d3ba5ddd819591a1555eafb5e5daa83
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-02-21 10:51:08 -0500

    adding login

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,5 +1,35 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBc1POzjZusSXvpbzP8L6noiu0C__5Fa_k',
+      {
+        method: 'POST',
+        body: JSON.stringify({
+          email: payload.email,
+          password: payload.password,
+          returnSecureToken: true,
+        }),
+      }
+    );
+
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      console.log(responseData);
+      const error = new Error(
+        responseData.message || 'Failed to authenticate. Check your login data.'
+      );
+      throw error;
+    }
+
+    console.log(responseData);
+    context.commit('setUer', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn,
+    });
+  },
+
   async signup(context, payload) {
     const response = await fetch(
       'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBc1POzjZusSXvpbzP8L6noiu0C__5Fa_k',
